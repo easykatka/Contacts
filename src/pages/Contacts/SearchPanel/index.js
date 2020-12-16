0bc7f1a44281366e3966576b83801325fdefe1b9
Easykatka <secondjump@bk.ru>
2020-12-17 02:51:29 +0300

    added css

--- src/pages/Contacts/SearchPanel/index.js
+++ src/pages/Contacts/SearchPanel/index.js
@@ -4,54 +4,68 @@ import { makeStyles } from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
 import SearchIcon from "@material-ui/icons/Search";
 import Paper from "@material-ui/core/Paper";
-import Box from "@material-ui/core/Box";
-import IconButton from '@material-ui/core/IconButton';
-import InputBase from '@material-ui/core/InputBase';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import Input from '@material-ui/core/Input';
+import Grid from "@material-ui/core/Grid";
+import IconButton from "@material-ui/core/IconButton";
+import Button from "@material-ui/core/Button";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import Select from "@material-ui/core/Select";
+import FormControl from "@material-ui/core/FormControl";
+import InputLabel from "@material-ui/core/InputLabel";
+import { Height } from "@material-ui/icons";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    root: {
-		padding: '2px 4px',
-		display: 'flex',
-		alignItems: 'center',
-		width: 400,
-		border : 'solid 1px'
-	  },
-    "& > *": {
-      margin: theme.spacing(2),
-    },
-  },
+	root: {
+		paddingBottom:theme.spacing(2),
+	},
+  input_item: { width: "100%" ,minWidth:theme.spacing(25)},
+  clear_btn: {
+	  width:'100%',
+  height:'100%',
+  }
 }));
 
 export const SearchPanel = () => {
   const classes = useStyles();
-
   return (
-	<Paper component="form" className={classes.root}>
-       <Input
-        className={classes.input}
-        placeholder="Search by full name"
-        inputProps={{ 'aria-label': 'search by full name' }}
-      />
-	  <SearchIcon />
-        
-    
-    </Paper>
+    <Grid container className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <OutlinedInput
+            size="small"
+            className={classes.input_item}
+            placeholder="Search by full name"
+            endAdornment={
+              <InputAdornment>
+                {" "}
+                <SearchIcon />
+              </InputAdornment>
+            }
+            size="small"
+          />
+        </Grid>
+        <Grid item xs>
+          <FormControl variant="outlined" className={classes.input_item}>
+            <InputLabel>Gender</InputLabel>
+            <Select label="Gender">
+              <MenuItem value={"all"}>All</MenuItem>
+              <MenuItem value={"man"}>Man</MenuItem>
+              <MenuItem value={"woman"}>Women</MenuItem>
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item xs>
+          <OutlinedInput
+            size="small"
+            className={classes.input_item}
+            placeholder="Nationality"
+            size="small"
+          />
+        </Grid>
+        <Grid item xs >
+          <Button className={classes.clear_btn}> Clear</Button>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
-
-
-// <MenuItem
-// id="outlined-basic"
-// size="small"
-// label="Gender"
-// variant="outlined"
-// />
-// <TextField
-// id="outlined-basic"
-// size="small"
-// label="Outlined"
-// variant="outlined"
-// />
--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -7,53 +7,63 @@ import { useContacts } from "./useContacts";
 import Typography from "@material-ui/core/Typography";
 import { ContactsTable } from "./ContactsTable";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import {ToggleDataViewMode} from './ToggleDataViewMode'
-import {DATA_VIEW_MODE} from './constants'
-import {useDataViewMode} from './useDataViewMode'
-import {SearchPanel} from './SearchPanel'
-import RefreshIcon from '@material-ui/icons/Refresh'
-import {useEffect} from 'react'
+import { ToggleDataViewMode } from "./ToggleDataViewMode";
+import { DATA_VIEW_MODE } from "./constants";
+import { useDataViewMode } from "./useDataViewMode";
+import { SearchPanel } from "./SearchPanel";
+import RefreshIcon from "@material-ui/icons/Refresh";
+import { useEffect } from "react";
+import IconButton from "@material-ui/core/IconButton";
 // styles
 const useStyles = makeStyles((theme) =>
   createStyles({
     root: {},
     headContainer: {
-		marginTop : theme.spacing(3),
-
-		
-    },
+      marginTop: theme.spacing(3),
+	},
+	refresh_button: {
+		marginRight: theme.spacing(1),
+	},
   })
 );
 //body
 export const Contacts = () => {
   const classes = useStyles();
-  const [getContacts,data,isLoading,isError] = useContacts();
+  const [getContacts, data, isLoading, isError] = useContacts();
   const [dataViewMode, setdataViewMode] = useDataViewMode();
   useEffect(() => {
-	  getContacts()
+    getContacts();
   }, []);
-//return
+  //return
   return (
     <Container className={classes.root}>
       <Grid container>
         <Grid item xs={12} className={classes.headContainer}>
-		<Box display="flex" justifyContent="space-between">
-          <Typography variant="h5" component="h1">
-            Contacts
-          </Typography>
-		  <Box  display="flex" >
-		  <Button onClick={() => getContacts()}>
-		  <RefreshIcon/>
-		  </Button>
-		  <ToggleDataViewMode dataViewMode={dataViewMode} setdataViewMode={setdataViewMode} DATA_VIEW_MODE={DATA_VIEW_MODE}/>
-		  </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Typography variant="h5" component="h1">
+              Contacts
+            </Typography>
+            <Box display="flex">
+              <IconButton className={classes.refresh_button}
+                aria-label="refresh"
+                
+                onClick={() => getContacts()}
+              >
+                <RefreshIcon />
+              </IconButton>
+              <ToggleDataViewMode
+                dataViewMode={dataViewMode}
+                setdataViewMode={setdataViewMode}
+                DATA_VIEW_MODE={DATA_VIEW_MODE}
+              />
+            </Box>
           </Box>
         </Grid>
 
-		<Grid item xs={12} className={classes.headContainer}>
-		<Box display="flex" justifyContent="space-between">
-		<SearchPanel/>
-        </Box>
+        <Grid item xs={12} className={classes.headContainer}>
+          <Box display="flex" justifyContent="space-between">
+            <SearchPanel />
+          </Box>
         </Grid>
 
         <Grid item xs={12}>
